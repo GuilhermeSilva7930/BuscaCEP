2fd82d20e073cccc2d5b6b22063965e67d58258d
Guilherme Ferreira <guilhermeferreira7930@gmail.com>
2022-03-10 16:51:35 -0300

    Busca CEP v0.4

--- src/App.jsx
+++ src/App.jsx
@@ -27,33 +27,37 @@ function App() {
           <label htmlFor="cep">CEP</label>
           <input className='form-control' required maxLength={9} onBlur={getCep} ref={cep} />
           <span className='btn btn-primary mt-2 mb-5'>Enviar</span>
-
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">#</th>
-                <th scope="col">Logradouro</th>
-                <th scope="col">Bairro</th>
-                <th scope="col">Localidade</th>
-                <th scope="col">Complemento</th>
-                <th scope="col">UF</th>
-                <th scope="col">DDD</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr key={endereco.cep}>
-                <th scope="row">1</th>
-                <td>{endereco.logradouro}</td>
-                <td>{endereco.bairro}</td>
-                <td>{endereco.localidade}</td>
-                <td>{endereco.complemento}</td>
-                <td>{endereco.uf}</td>
-                <td>{endereco.ddd}</td>
-              </tr>
-            </tbody>
-          </table>
+        </div>
+        <div className="row">
+          <div className="col boxData">
+            <table className="table">
+              <thead>
+                <tr>
+                  <th scope="col">#</th>
+                  <th scope="col">Logradouro</th>
+                  <th scope="col">Bairro</th>
+                  <th scope="col">Localidade</th>
+                  <th scope="col">Complemento</th>
+                  <th scope="col">UF</th>
+                  <th scope="col">DDD</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr key={endereco.cep}>
+                  <th scope="row">1</th>
+                  <td>{endereco.logradouro}</td>
+                  <td>{endereco.bairro}</td>
+                  <td>{endereco.localidade}</td>
+                  <td>{endereco.complemento}</td>
+                  <td>{endereco.uf}</td>
+                  <td>{endereco.ddd}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
+
     </main>
   );
 }
